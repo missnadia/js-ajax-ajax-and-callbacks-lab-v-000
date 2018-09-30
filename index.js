1c7a79e6414639d98f7207ecb2742a9debd82172
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-30 05:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-var displayError = () => {
-  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
 }
-
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
 var searchRepositories = () => {
   const searchTerms = $('#searchTerms').val()
-  const url = `https://api.github.com/search/repositories?q=${searchTerms}`
-  $.get(url).done(function(data) {
-    $('#results').html(data);
-  }).fail(function(error) {
-    displayError();
-  });
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
 }
-
-function showCommits(el) {
-  const url = `https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`
-  $.get(url).done(function(data) {
-    $('#details').html(data);
-  }).fail(function(error) {
-    displayError()
-  });
-}
-
-$(document).ready(function (){
-});
