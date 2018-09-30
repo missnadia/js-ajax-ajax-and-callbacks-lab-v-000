de7495709e2bebaae08a2d7e9761caf699fd9601
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-30 04:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function (){
     $('#errors').html("I'm sorry, there's been an error. Please try again.");
   }
 
-  var searchRepositories = () => {
+  function searchRepositories() {
     const searchTerms = $('#searchTerms').val()
     const url = `https://api.github.com/search/repositories?q=${searchTerms}`
     $.get(url, function(data) {
