2dcb887ef9f2f6fe74e25f0ed9533f7edd9bcd53
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-30 04:45:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 $(document).ready(function (){
+  var displayError = () => {
+    $('#errors').html("I'm sorry, there's been an error. Please try again.");
+  }
+
+  var searchRepositories = () => {
+    const searchTerms = $('#searchTerms').val()
+    const url = `https://api.github.com/search/repositories?q=${searchTerms}`
+    $.get(url, function(data) {
+      $('#results').html(data);
+    }).fail(function(error) {
+      displayError();
+    });
+  }
+
+  var showCommits = (el) => {
+    const url = `https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`
+    $.get(url, function(data) {
+      $('#details').html(data);
+    }).fail(function(error) {
+      displayError()
+    });
+  }
 });
